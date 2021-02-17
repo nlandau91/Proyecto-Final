f0991b1681fb899671230f66394c070dcc3b79f9
nico <nlandau91@gmail.com>
2021-02-16 23:16:35 -0300

    homografia, empieza a andar...

--- Proyecto-Final/comparator.cpp
+++ Proyecto-Final/comparator.cpp
@@ -71,7 +71,8 @@ bool Comparator::compare(const cv::Mat &query_descriptors, const cv::Mat &train_
     {
     case ORB:
     {
-        matches = find_matches(query_descriptors,train_descriptors,cv::NORM_HAMMING,0.75);
+        //matches = find_matches(query_descriptors,train_descriptors,cv::NORM_HAMMING,0.75);
+        matches = find_matches(query_descriptors,train_descriptors,cv::NORM_HAMMING);
         break;
     }
     case SURF:
@@ -84,28 +85,76 @@ bool Comparator::compare(const cv::Mat &query_descriptors, const cv::Mat &train_
     bool comparation = false;
     if(matches.size() > 1)
     {
-        //armamos los arcos
-        double min_dist = 15.0; // minima distancia que debe tener un arco
-        std::vector<Edge> query_edges = get_edges(query_keypoints, min_dist);
-        std::vector<Edge> train_edges = get_edges(train_keypoints, min_dist);
-
-        //comparamos los arcos
-        for(const Edge &e1 : query_edges)
+        //metodo de edge matching
+        //arma arcos que contienen distancia y angulo de cada nodo(minutiae)
+        //en teoria es invariante a traslacion y rotacion, por lo que sirve como metodo de matching
+        if(edge_matching)
         {
-          qDebug() << "dist: " << e1.dist;
-//            qDebug() << "angle: " << e1.angle;
-//            qDebug() << "alpha: " << e1.alpha;
-//            qDebug() << "beta: " << e1.beta;
-            for(const Edge &e2 : train_edges)
+            //armamos los arcos
+            double min_dist = 15.0; // minima distancia que debe tener un arco
+            std::vector<Edge> query_edges = get_edges(query_keypoints, min_dist);
+            std::vector<Edge> train_edges = get_edges(train_keypoints, min_dist);
+
+            //comparamos los arcos
+            for(const Edge &e1 : query_edges)
             {
-                comparation = e1.compare(e2, edge_dist, edge_angle);
-
-                if(comparation)qDebug() << "Comparation: " << comparation;
-
+                qDebug() << "dist: " << e1.dist;
+                for(const Edge &e2 : train_edges)
+                {
+                    comparation = e1.compare(e2, edge_dist, edge_angle);
+                    if(comparation)qDebug() << "Comparation: " << comparation;
+                }
             }
         }
 
+        //filtramos los matches
+        std::vector<cv::DMatch> good_matches = matches;
+        std::sort(good_matches.begin(), good_matches.end(),
+                  [](cv::DMatch const & a, cv::DMatch const & b) -> bool { return a.distance < b.distance; } );
+        const float GOOD_MATCH_PERCENT = 0.5f;
+        const int numGoodMatches = good_matches.size() * GOOD_MATCH_PERCENT;
+        good_matches.erase(good_matches.begin()+numGoodMatches, good_matches.end());
+
+        std::vector<cv::Point2f> q_points;
+        std::vector<cv::Point2f> t_points;
+        for(const cv::DMatch &m : good_matches)
+        {
+            cv::Point2f p1(
+                        query_keypoints.at<float>(m.queryIdx,0),
+                        query_keypoints.at<float>(m.queryIdx,1));
+            cv::Point2f p2(
+                        train_keypoints.at<float>(m.trainIdx,0),
+                        train_keypoints.at<float>(m.trainIdx,1));
+            q_points.push_back(p1);
+            t_points.push_back(p2);
+        }
+        //registramos la huella dactilar, para que tenga la misma traslacion y rotacion
+        if(q_points.size() > 3)
+        {
+            cv::Mat H = cv::findHomography(q_points,t_points,cv::RANSAC);
+
+            if(!H.empty())
+            {
+                cv::Mat in = cv::imread("/home/nico/Proyecto-Final/res/FVC2002/DB1_B/101_2.tif",cv::IMREAD_GRAYSCALE);
+                cv::Mat out;
+                cv::warpPerspective(in,out,H,in.size());
+                cv::imwrite("OUT.TIF",out);
+                //            //obtenemos angulo de rotacion de la homografia
+                //            //descomposicion SVD de la submatriz 2x2 superior izquierda
+                //            cv::Mat w,u,vt;
+                //            cv::SVD::compute(H(cv::Rect(0,0,2,2)),w,u,vt);
+                //            cv::Mat R = u * vt;
+
+                //            float angle = std::atan2(R.at<float>(1,0),R.at<float>(0,0)) * 180.0 / M_PI;
+                float angle2 = - std::atan2(H.at<float>(0,1),H.at<float>(0,0)) * 180.0 / M_PI;
+                qDebug() << "angle2: " << angle2;
+            }
+        }
+
+
+        //metodo basico de matching, utilizando simplemente la cantidad de matches encontrados entre minutiae
         double score = (double)matches.size()/std::max(query_descriptors.rows,train_descriptors.rows);
+        qDebug() << "score: " << score;
         comparation = score > threshold;
 
     }
